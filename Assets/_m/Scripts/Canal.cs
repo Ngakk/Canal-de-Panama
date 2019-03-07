5fdb34566d1b6ca1836bf6b20612bfe98cd5351e
Ngakk <32682022+Ngakk@users.noreply.github.com>
2019-03-07 10:46:33 -0600

    Initial commit

---
+++ Assets/_m/Scripts/Canal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canal : MonoBehaviour
+{
+    public GameObject gatePrefab;
+    public GameObject waterPrefab;
+    public GameObject boatPrefab;
+
+    public int NumeroDeCompuertas = 3;
+    public float wOffset = 10;
+    public float hOffset = 3;
+
+    private List<Gate> gates;
+    private List<Water> waters;
+    private Vector3 start;
+    private Vector3 end;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Creating start water
+        GameObject go = Instantiate(waterPrefab, Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_m/Scripts/Gate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    bool isOpen;
+
+    public void Open()
+    {
+        if (!isOpen)
+        {
+            LeanTween.moveLocal(gameObject, new Vector3(0, 0, transform.localScale.z), 0.5f).setOnComplete(OnOpen);
+        }
+    }
+
+    public void OnOpen()
+    {
+
+    }
+
+    public void Close()
+    {
+        if (isOpen)
+        {
+            LeanTween.moveLocal(gameObject, new Vector3(0, 0, -transform.localScale.z), 0.5f).setOnComplete(OnClose);
+        }
+    }
+
+    public void OnClose()
+    {
+
+    }
+}
