d2220350d260b1529623e40a2bd7ed97c2c33786
Ngakk <32682022+Ngakk@users.noreply.github.com>
2019-03-14 10:57:39 -0600

    asd

--- Assets/_m/Scripts/Boat.cs
+++ Assets/_m/Scripts/Boat.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Boat : MonoBehaviour
 {
+    public float Speed = 2.0f;
+
+    private Rigidbody rigi;
+    private Vector3 dir = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigi = GetComponent<Rigidbody>();
+        dir = transform.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Stop();
+        }
+    }
+
+    public void Stop()
+    {
+        Debug.Log("Stop");
+        rigi.velocity = Vector3.zero;
+    }
+
+    public void Move()
+    {
+        Debug.Log("Start");
+        rigi.velocity = dir.normalized * Speed;
     }
 }
